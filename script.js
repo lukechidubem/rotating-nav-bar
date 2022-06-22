3057d772f9b1591ebfb25b404f334edc01179950
lukechidubem <lukechidubem@gmail.com>
2022-06-22 07:59:36 -0700

    Added light and dark mode

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ open.addEventListener('click', function () {
 close.addEventListener('click', function () {
   container.classList.remove('show-menu');
 });
+
+// Appearance (Light and Dark mode)
+const sun = document.getElementById('sun');
+const moon = document.getElementById('moon');
+
+moon.addEventListener('click', function () {
+  container.classList.add('dark');
+  moon.style.display = 'none';
+  sun.style.display = 'block';
+});
+
+sun.addEventListener('click', function () {
+  container.style.transition = '0.5s ease-in';
+  container.classList.remove('dark');
+  moon.style.display = 'block';
+  sun.style.display = 'none';
+});
